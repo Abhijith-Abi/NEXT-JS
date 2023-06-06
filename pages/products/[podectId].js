02a2e69123b3222c6906ae52c66094b61b5a16cd
abhijith12345 <abhijith@steyp.com>
2023-06-06 13:39:29 +0530

    Nexted Router

---
+++ pages/products/[podectId].js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+function PoroductIdPage() {
+    const router = useRouter();
+    const productId = router.query.podectId;
+
+    return (
+        <>
+            <h1>This is the product id shown page</h1>
+            <h2>
+                Your product id is{" "}
+                <small style={{ color: "lightblue", fontStyle: "italic" }}>
+                    {productId}
+                </small>
+            </h2>
+        </>
+    );
+}
+
+export default PoroductIdPage;
---
+++ pages/products/index.js
@@ -0,0 +1,13 @@
+function ProductList() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>Product Lists</h1>
+            <br />
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 2</h2>
+        </div>
+    );
+}
+
+export default ProductList;
